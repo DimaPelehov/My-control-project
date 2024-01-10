25712bb832cc1a53df3bcfca07b31bd7ff9517ea
Dmytro <pelexov.mitya@gmail.com>
2024-01-10 14:10:10 +0200

    add AppBar from Material UI and install iconsmaterial

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,5 +1,17 @@
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
 type Props = {}
+
 const App = (props: Props) => {
-    return <div>App</div>
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
 }
+
 export default App
